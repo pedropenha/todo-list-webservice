7facb4595aa77cf23e94936f167fee796364aa54
Pedro Penha <pedro.penha.martins@gmail.com>
2022-07-27 22:53:32 -0300

    creating methods for rest api

--- app/Controllers/Task.php
+++ app/Controllers/Task.php
@@ -6,5 +6,188 @@ use CodeIgniter\RESTful\ResourceController;
 
 class Task extends ResourceController
 {
+    public function findAllTasks()
+    {
+        $taskModel = new \App\Models\TasksModel();
 
+        try{
+            $data = $taskModel->findAll();
+            if(sizeof($data) > 0){
+                $response = [
+                    'response' => 'success',
+                    'tasks' => $data,
+                ];
+            }else{
+                $response = [
+                    'response' => 'error',
+                    'error' => $taskModel->errors()
+                ];
+            }
+        }catch (\Exception $e){
+            $response = [
+                'response' => 'error',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
+
+    public function findByIdTask($id)
+    {
+        $taskModel = new \App\Models\TasksModel();
+
+        try{
+            $data = $taskModel->find($id);
+            if(sizeof($data) > 0){
+                $response = [
+                    'response' => 'success',
+                    'tasks' => $data,
+                ];
+            }else{
+                $response = [
+                    'response' => 'error',
+                    'error' => $taskModel->errors()
+                ];
+            }
+        }catch (\Exception $e){
+            $response = [
+                'response' => 'error',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
+
+    public function findByIdTodo($idTodo){
+        $taskModel = new \App\Models\TasksModel();
+
+        try{
+            $data = $taskModel->findAllByIdTodo($idTodo);
+            if(sizeof($data) > 0){
+                $response = [
+                    'response' => 'success',
+                    'tasks' => $data,
+                ];
+            }else{
+                $response = [
+                    'response' => 'error',
+                    'error' => $taskModel->errors()
+                ];
+            }
+        }catch (\Exception $e){
+            $response = [
+                'response' => 'error',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
+
+    public function saveTask()
+    {
+        $taskModel = new \App\Models\TasksModel();
+
+        try{
+            $newTask = [
+                'content' => $this->request->getPost('content'),
+                'idTodo' => $this->request->getPost('idTodo')
+            ];
+
+            if($taskModel->insert($newTask)){
+                $response = [
+                    'response' => 'success',
+                    'msg' => 'task created with success'
+                ];
+            }else{
+                $response = [
+                    'response' => 'error',
+                    'msg' => 'error create task',
+                    'error' => $taskModel->errors()
+                ];
+            }
+        }catch (\Exception $e){
+            $response = [
+                'response' => 'error',
+                'msg' => 'error create task',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+        return $this->response->setJSON($response);
+    }
+
+    public function updateTask()
+    {
+        $taskModel = new \App\Models\TasksModel();
+
+        try{
+            $id = $this->request->getVar('id');
+
+            $newTask = [
+                'content' => $this->request->getVar('content'),
+            ];
+
+            if($taskModel->update($id, $newTask)){
+                $response = [
+                    'response' => 'success',
+                    'msg' => 'task updated with success'
+                ];
+            }else {
+                $response = [
+                    'response' => 'error',
+                    'msg' => 'error on update task',
+                    'error' => $taskModel->errors()
+                ];
+            }
+        }catch (\Exception $e){
+            $response = [
+                'response' => 'error',
+                'msg' => 'error on update task',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
+
+    public function deleteTask($id){
+        $taskModel = new \App\Models\TasksModel();
+
+        try{
+            if($taskModel->delete($id)){
+                $response = [
+                    'response' => 'success',
+                    'msg' => 'task deleted with success'
+                ];
+            }else {
+                $response = [
+                    'response' => 'error',
+                    'msg' => 'error on deleted task',
+                    'error' => $taskModel->errors()
+                ];
+            }
+        }catch (\Exception $e){
+            $response = [
+                'response' => 'error',
+                'msg' => 'error on deleted task',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
 }
--- app/Controllers/Todo.php
+++ app/Controllers/Todo.php
@@ -6,5 +6,119 @@ use CodeIgniter\RESTful\ResourceController;
 
 class Todo extends ResourceController
 {
+    public function findAllTodo()
+    {
+        $todoModel = new \App\Models\TodoModel();
 
+        $data = $todoModel->findAll();
+
+        return $this->response->setJSON($data);
+    }
+
+    public function findByIdTodo($id)
+    {
+        $todoModel = new \App\Models\TodoModel();
+
+        $data = $todoModel->find($id);
+
+        return $this->response->setJSON($data);
+    }
+
+    public function saveTodo()
+    {
+        $todoModel = new \App\Models\TodoModel();
+
+        try{
+            $newTodo = [
+                'title' => $this->request->getPost('title')
+            ];
+
+            if($todoModel->save($newTodo)){
+                $response = [
+                    'response' => 'success',
+                    'msg' => 'todo list created with success'
+                ];
+            }else{
+                $response = [
+                    'response' => 'error',
+                    'msg' => 'error create todo list',
+                    'error' => $todoModel->errors()
+                ];
+            }
+        }catch (Exception $e){
+            $response = [
+                'response' => 'error',
+                'msg' => 'error create todo list',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+        return $this->response->setJSON($response);
+    }
+
+    public function updateTodo()
+    {
+        $todoModel = new \App\Models\TodoModel();
+
+        try{
+            $id = $this->request->getVar('id');
+
+            $newTodo = [
+                'title' => $this->request->getVar('title'),
+            ];
+
+            if($todoModel->update($id, $newTodo)){
+                $response = [
+                    'response' => 'success',
+                    'msg' => 'todo list updated with success'
+                ];
+            }else {
+                $response = [
+                    'response' => 'error',
+                    'msg' => 'error on update todo list',
+                    'error' => $todoModel->errors()
+                ];
+            }
+        }catch (Exception $e){
+            $response = [
+                'response' => 'error',
+                'msg' => 'error on update todo list',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
+
+    public function deleteTodo($id){
+        $todoModel = new \App\Models\TodoModel();
+
+        try{
+            if($todoModel->delete($id)){
+                $response = [
+                    'response' => 'success',
+                    'msg' => 'todo list deleted with success'
+                ];
+            }else {
+                $response = [
+                    'response' => 'error',
+                    'msg' => 'error on deleted todo list',
+                    'error' => $todoModel->errors()
+                ];
+            }
+        }catch (Exception $e){
+            $response = [
+                'response' => 'error',
+                'msg' => 'error on deleted todo list',
+                'errors' => [
+                    'exception' => $e->getMessage()
+                ]
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
 }
